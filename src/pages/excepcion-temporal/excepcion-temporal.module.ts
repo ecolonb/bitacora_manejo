01b74195ad3bdb45074da2f1a9dbc606ada67a6b
Edgar Colón Barrientos <ecolon@copiloto.com.mx>
2018-08-01 08:14:13 -0500

    Se agregaron las nuevas paginas: configuración, excepcion-temporal

---
+++ src/pages/excepcion-temporal/excepcion-temporal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ExcepcionTemporalPage } from './excepcion-temporal';
+
+@NgModule({
+  declarations: [
+    ExcepcionTemporalPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ExcepcionTemporalPage),
+  ],
+})
+export class ExcepcionTemporalPageModule {}
